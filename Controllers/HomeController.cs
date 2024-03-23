0271989054d1e3c090d8f856629caa1e1ef86f1d
Andrew Naumann <andnau702@gmail.com>
2024-03-23 00:39:37 -0600

    chunky boy

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Mission11_Naumann.Models;
+using Mission11_Naumann.Models.ViewModels;
 using System.Diagnostics;
 
 namespace Mission11_Naumann.Controllers
@@ -17,12 +18,23 @@ namespace Mission11_Naumann.Controllers
         {
             int pageSize = 10;
 
-            var bookData = _repo.Books
-                .OrderBy(x => x.Title)
-                .Skip((pageNum - 1) * pageSize)
-                .Take(pageSize);
+            var book = new BooksListViewModel
+            {
+                Books = _repo.Books
+                    .OrderBy(x => x.Title)
+                    .Skip((pageNum - 1) * pageSize)
+                    .Take(pageSize),
 
-            return View(bookData);
+                PaginationInfo = new PaginationInfo
+                {
+                    CurrentPage = pageNum,
+                    ItemsPerPage = pageSize,
+                    TotalItems = _repo.Books.Count()
+
+                }
+            };
+                
+            return View(book);
         }
     }
 }
